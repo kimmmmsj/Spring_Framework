e9b214bdcaba40edb6db4811b1f0e8f81c24fc04
kimmmmsj <86100785+kimmmmsj@users.noreply.github.com>
2022-01-14 18:03:47 +0900

    123

---
+++ SpringBoot/startboot/src/test/java/hello/startboot/StartbootApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.startboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
