5c97ac317e79da8ef537cac563a7f728c04cc7f1
kimmmmsj <86100785+kimmmmsj@users.noreply.github.com>
2022-01-17 18:54:51 +0900

    123

---
+++ 3. 스프링 MVC 2편/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,162 @@
+package hello.thymeleaf.basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    //1
+    @GetMapping("/text-basic")
+    public String textBasic(Model model){
+        model.addAttribute("data", "hello spring!");
+        return "basic/text-basic";
+    }
+
+    //2
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data", "hello <b>spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+    //3
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+
+    //4
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+
+    //5
+    @GetMapping("/date")
+    public String date(Model model){
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    //6
+    @GetMapping("/link")
+    public String link(Model model){
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "/basic/link";
+    }
+
+    //7
+    @GetMapping("/literal")
+    public String literal(Model model){
+        model.addAttribute("data", "Spring!");
+        return "/basic/literal";
+    }
+
+    //8
+    @GetMapping("/operation")
+    public String operation(Model model){
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "/basic/operation";
+    }
+
+    //9
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "/basic/attribute";
+    }
+
+    //10
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "/basic/each";
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users", list);
+    }
+
+    //11
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "/basic/condition";
+    }
+
+    //12 -> comments.html의 2번 주석을 주로 쓸 것이다!
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data", "Spring!");
+        return "/basic/comments";
+    }
+
+    //13
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "/basic/block";
+    }
+
+    //14
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+}
