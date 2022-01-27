47683bda9f4355b0d8658c294ead8f93264c0afa
김성재 <tjdwo1994@gmail.com>
2022-01-27 14:55:36 +0900

    123

--- 3. 스프링 MVC 2편/exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ 3. 스프링 MVC 2편/exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -18,11 +18,16 @@ public class ServletExController {
 
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
-        response.sendError(404, "404 오류!!");
+        response.sendError(404, "404 오류!");
+    }
+
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400 오류!");
     }
 
     @GetMapping("/error-500")
-    public void error500(HttpServletResponse response) throws IOException{
+    public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
     }
 }
