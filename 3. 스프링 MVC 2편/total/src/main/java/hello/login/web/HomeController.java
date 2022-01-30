46ede0e4093f99e2723209fbf576e9598e66adc2
김성재 <tjdwo1994@gmail.com>
2022-01-30 12:35:00 +0900

    123

--- 3. 스프링 MVC 2편/total/src/main/java/hello/login/web/HomeController.java
+++ 3. 스프링 MVC 2편/total/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,46 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.web.argumentresolver.Login;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    //SessionAttribute 어노테이션 사용한 예제
+    //@GetMapping("/")
+    public String homeLoginSpring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    //@Login이라는 어노테이션을 만들어서 사용한 예제(argumentresolver패키지에 있음)
     @GetMapping("/")
-    public String home() {
-        return "redirect:/items";
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model) {
+
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
